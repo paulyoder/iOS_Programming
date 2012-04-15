f28e61d9a52fab00fecce6595fda3ede04fd465f
Paul Yoder <paulyoder@gmail.com>
2012-04-15 06:56:59 -0500

    created Whereami

---
+++ Whereami/Whereami/WhereamiViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhereamiViewController.h
+//  Whereami
+//
+//  Created by Paul Yoder on 4/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhereamiViewController : UIViewController
+
+@end
