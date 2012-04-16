95f71340bff280b9bd9cad4c9c4ba8deb6fe8659
Paul Yoder <paulyoder@gmail.com>
2012-04-16 16:58:05 -0500

    added Hypnosister and halfway through chapter 6

---
+++ Hypnosister/Hypnosister/HypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  HypnosisView.h
+//  Hypnosister
+//
+//  Created by Paul Yoder on 4/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HypnosisView : UIView
+
+@end
---
+++ Hypnosister/Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Paul Yoder on 4/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
